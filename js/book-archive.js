6c2cf84ab841f399c4e25914bcd062448bb53574
saifulislammunna <saifulmunna28@gmail.com>
2021-09-02 18:13:09 +0600

    added

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,3 +1,4 @@
+ 
 /* search book function */
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
@@ -5,20 +6,34 @@ const searchBook = () => {
     // console.log(searchText);
     /* clear data */
     searchField.value = '';
+     
+    if(searchText =='' ){
+      
 
-     /* load data */ 
+       
+      
+    }
+    else{
+       /* load data */ 
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySearchResult(data.docs));
+    .then(data => displaySearchResult(data.docs))
+      
+
+    }
+
+    
 }
  
+ 
+
 /* display result function */
 const displaySearchResult = docs => {
      
     const searchResult = document.getElementById('search-result');
-     
+    searchResult.textContent = ''; 
      
     docs.forEach(doc => {
         console.log(doc)
@@ -26,13 +41,13 @@ const displaySearchResult = docs => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${doc.title}</h5>
+          <img src=" https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
+         <div class="card-body">
+          <h1 class="card-title">${doc.title}</h1>
           <p class="card-text"> ${doc.author_name}</p>
           <h2 class="card-text"> ${doc.first_publish_year}</h2>
 
-        </div>
+         </div>
       </div>
         
         `;
